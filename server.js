bfa0f6b7a1e10ca1fd618cb8b469226238b2f22a
Leon <LAnneken09@gmail.com>
2023-02-26 20:16:43 +0100

    Added the standard route

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const fs = require("fs");
 const path = require("path");
 // App Einstellungen
 app.listen(2000, () => {
-    fs.readFile("./Daten/Signatur/signatur.txt", function(err, data) {
-        console.log(String(data));
-    });
+    
 });
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -17,7 +15,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "Seiten/Views"));
 app.use(express.static(path.join(__dirname, "Seiten/Public")))
 // Seiten Routen
-
+app.get("/", (req, res) => {
+    res.render("index")
+})
 // API Routen
 
 // Intern Routen
